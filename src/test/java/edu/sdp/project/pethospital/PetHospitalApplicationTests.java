ab63c86acb5ca92f2365397a20e0d37d7b603af3
makise <10175101106@stu.ecnu.edu.cn>
2021-03-08 15:15:27 +0800

    feat: create springboot project and add git

---
+++ src/test/java/edu/sdp/project/pethospital/PetHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sdp.project.pethospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
