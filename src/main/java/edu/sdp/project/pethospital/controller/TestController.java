28f11893aed83d278619cd891c558dcdddc6754d
HarveyLo <my751721909@gmail.com>
2021-04-12 21:12:55 +0800

    Add Test Function is now online

--- src/main/java/edu/sdp/project/pethospital/controller/TestController.java
+++ src/main/java/edu/sdp/project/pethospital/controller/TestController.java
@@ -1,10 +1,11 @@
 package edu.sdp.project.pethospital.controller;
 
+import edu.sdp.project.pethospital.entity.Question;
 import edu.sdp.project.pethospital.entity.ResponseMsg;
 import edu.sdp.project.pethospital.entity.Test;
+import edu.sdp.project.pethospital.service.OptionUserService;
 import edu.sdp.project.pethospital.service.TestService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.annotations.Delete;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,9 +16,11 @@ import java.util.Map;
 @CrossOrigin(origins="*")
 public class TestController {
     private final TestService testService;
+    private final OptionUserService optionUserService;
 
-    public TestController(TestService testService) {
+    public TestController(TestService testService, OptionUserService optionUserService) {
         this.testService = testService;
+        this.optionUserService = optionUserService;
     }
 
     @ResponseBody
@@ -83,11 +86,28 @@ public class TestController {
 
     /**
      * test的新增应该只有在用户开始一场考试时才生成，因此不属于管理员的功能
-     * 此接口应该属于用户功能部分，暂时留空
+     * 调用该接口会在test表中插入一条新记录，并返回一套试卷（一个Question列表）和新插入的testId
+     * 分别通过result和testId拿取
+     * 此接口会进行一些条件判断
+     * 返回404表示该用户无权限参加考试
+     * 返回400表示该用户已经参加过该考试
+     * 返回500表示该用户可以参加该考试但是目前事件已经超过或者还未到考试时间
      */
     @ResponseBody
-    @PutMapping("/exam")
-    ResponseMsg startTest(){
-        return null;
+    @PutMapping("/exam/{testOptionId}/user/{userId}")
+    ResponseMsg startTest(@PathVariable("testOptionId") int testOptionId,@PathVariable("userId") int userId){
+        ResponseMsg msg = new ResponseMsg();
+        msg.setStatus(404);
+        if(!optionUserService.checkId(testOptionId,userId)) return msg;
+        msg.setStatus(400);
+        if(testService.checkTest(userId,testOptionId)) return msg;
+        msg.setStatus(500);
+        if(!testService.checkDate(testOptionId)) return msg;
+        List<Question> result = testService.getNewTest(userId,testOptionId);
+        int testId = testService.addTest(userId,testOptionId);
+        if(result!=null&&result.size()>0&&testId>0) msg.setStatus(200);
+        msg.getResponseMap().put("result",result);
+        msg.getResponseMap().put("testId",testId);
+        return msg;
     }
 }
--- src/main/java/edu/sdp/project/pethospital/service/TestService.java
+++ src/main/java/edu/sdp/project/pethospital/service/TestService.java
@@ -3,22 +3,30 @@ package edu.sdp.project.pethospital.service;
 import edu.sdp.project.pethospital.entity.Question;
 import edu.sdp.project.pethospital.entity.Test;
 import edu.sdp.project.pethospital.entity.TestOption;
+import edu.sdp.project.pethospital.mapper.OptionUserMapper;
+import edu.sdp.project.pethospital.mapper.QuestionMapper;
 import edu.sdp.project.pethospital.mapper.TestMapper;
 import edu.sdp.project.pethospital.mapper.TestOptionMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 @Slf4j
 public class TestService {
     private final TestMapper testMapper;
     private final TestOptionMapper testOptionMapper;
+    private final OptionUserMapper optionUserMapper;
+    private final QuestionMapper questionMapper;
 
-    public TestService(TestMapper testMapper, TestOptionMapper testOptionMapper) {
+    public TestService(TestMapper testMapper, TestOptionMapper testOptionMapper, OptionUserMapper optionUserMapper, QuestionMapper questionMapper) {
         this.testMapper = testMapper;
         this.testOptionMapper = testOptionMapper;
+        this.optionUserMapper = optionUserMapper;
+        this.questionMapper = questionMapper;
     }
     public List<Test> getAllTests(){
         return testMapper.selectAllTest();
@@ -29,11 +37,27 @@ public class TestService {
     public List<Test> getAllTestsByOptionId(int testOptionId){
         return testMapper.selectByTestOptionId(testOptionId);
     }
+    public boolean checkDate(int testOptionId){
+        long now = System.currentTimeMillis();
+        TestOption option = testOptionMapper.selectById(testOptionId);
+        long start  = option.getStartDate().getTime();
+        long duration = (long)option.getDuration()*60*1000;
+        log.info(duration+"");
+        log.info(start+"");
+        log.info(now+"");
+        return now>start&&now<(start+duration);
+    }
     public Test getTestById(int testId){
         return testMapper.selectById(testId);
     }
-    public int addTest(Test test){
-        return testMapper.insert(test);
+    public int addTest(int userId, int testOptionId){
+        Test test = new Test();
+        test.setUserId(userId);
+        test.setTestOptionId(testOptionId);
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        test.setBeginDate(new Date());
+        if(testMapper.insert(test)>0) return test.getTestId();
+        return 0;
     }
     public int deleteTestById(int testId){
         return testMapper.deleteById(testId);
@@ -45,10 +69,41 @@ public class TestService {
         Test test = testMapper.selectById(testId);
         return test!=null;
     }
-//    public List<Question> getNewTest(int userId,int testOptionId){
-//        TestOption testOption = testOptionMapper.selectById(testOptionId);
-//        List<Question> selects = null;
-//        List<Question> judges = null;
-//        List<Question> qas = null;
-//    }
+    public boolean checkTest(int userId,int testOptionId){
+        Test test = testMapper.selectByUerIdOptionId(userId,testOptionId);
+        return test!=null;
+    }
+    public List<Question> getNewTest(int userId, int testOptionId){
+        TestOption option = testOptionMapper.selectById(testOptionId);
+        List<Question> result = new ArrayList<>();
+        List<Question> tem=null;
+        tem = getQess("select",option.getSelectNum(),option.getSelectTag());
+        if(tem!=null) result.addAll(tem);
+        tem = getQess("judge",option.getJudgeNum(),option.getJudgeTag());
+        if(tem!=null) result.addAll(tem);
+        tem = getQess("qa",option.getQaNum(),option.getQaTag());
+        if(tem!=null) result.addAll(tem);
+        return result;
+    }
+    List<Question> getQess(String type,int num,String tag){
+        List<Question> tem = null;
+        if(num==0) return null;
+        if(tag!=null&&!tag.equals(""))
+            tem = questionMapper.selectQuestionByTagAndType(type,tag);
+        else tem = questionMapper.selectQuestionByType(type);
+        if(tem==null) return null;
+        if(tem.size()<num) return null;
+        if(tem.size()==num) return tem;
+        HashSet<Integer> set = new HashSet<>();
+        Random r = new Random();
+        while(set.size()<num){
+            int next = r.nextInt(tem.size());
+            set.add(next);
+        }
+        List<Question> result = new ArrayList<>();
+        for (Integer integer : set) {
+            result.add(tem.get(integer));
+        }
+        return result;
+    }
 }
