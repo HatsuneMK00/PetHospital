7b1afdbdd35d4b3cf7b7c6d81aaeaa6d71b52475
Liu-Ruiyang <1094635582@qq.com>
2021-04-19 19:52:34 +0800

    completed MedicineControllerTest.

--- src/test/java/edu/sdp/project/pethospital/controller/MedicineControllerTest.java
+++ src/test/java/edu/sdp/project/pethospital/controller/MedicineControllerTest.java
@@ -3,6 +3,7 @@ package edu.sdp.project.pethospital.controller;
 import edu.sdp.project.pethospital.entity.Medicine;
 import edu.sdp.project.pethospital.entity.ResponseMsg;
 import edu.sdp.project.pethospital.service.MedicineService;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,7 @@ import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 
@@ -44,16 +46,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
+
 public class MedicineControllerTest {
     private MedicineService medicineService;
     private MockMvc mockMvc;
     private static ObjectMapper mapper;
+
     @BeforeEach
     void setUp() {
         medicineService = mock(MedicineService.class);
         mockMvc = standaloneSetup(new MedicineController(medicineService)).build();
         mapper = new ObjectMapper();
- 
+
     }
 
     @Test
@@ -91,7 +95,7 @@ public class MedicineControllerTest {
     }
 
     @Test
-    void null_path_when_fetch_vaccine() throws Exception {
+    void null_path_when_fetch_medicine() throws Exception {
         when(medicineService.getMedicine(101)).thenReturn(null);
         mockMvc.perform(put("/admin/structure/medicine/{medId}", 101)).andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value(404));
@@ -99,15 +103,88 @@ public class MedicineControllerTest {
     }
 
     @Test
-    void happy_path_when_add_vaccine() throws Exception {
-        Medicine medicine = new Medicine(101, "med1", "descriptiontest1");
-        when(medicineService.addMedicine(ArgumentMatchers.any())).thenReturn(1);
-        String json = mapper.writeValueAsString(medicine);
-        mockMvc.perform(put("/admin/structure/medicine").contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
-            .content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-            .andExpect(jsonPath("$.status").value(200));
+    void happy_path_when_add_medicine() throws Exception {
+        Medicine medicine = new Medicine(101, "med1", "des1");
+
+        when(medicineService.addMedicine(medicine)).thenReturn(1);
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("medName", "med1");
+        requestParams.add("medDescrip", "des1");
+        mockMvc.perform(put("/admin/structure/medicine").params(requestParams)).andExpect(status().isOk());
 
         // verify(medicineService).addMedicine(medicine);
         // verifyNoMoreInteractions(medicineService);
+        // returns unexpected 404
     }
+
+    @Test
+    void null_path_when_add_medicine() throws Exception {
+        Medicine medicine = new Medicine(101, "med1", "des1");
+
+        when(medicineService.addMedicine(medicine)).thenReturn(-1);
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+
+        requestParams.add("medName", "med1");
+        requestParams.add("medDescrip", "des1");
+        mockMvc.perform(put("/admin/structure/medicine").params(requestParams)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(404));
+    }
+
+    @Test
+    void happy_path_when_delete_medicine() throws Exception {
+        int medId = 101;
+        when(medicineService.checkId(medId)).thenReturn(true);
+        when(medicineService.deleteMedicine(medId)).thenReturn(1);
+        mockMvc.perform(delete("/admin/structure/medicine").param("medId", "101")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(200));
+        verify(medicineService).deleteMedicine(medId);
+    }
+
+    @Test
+    void null_path_when_delete_medicine() throws Exception {
+        int medId = 101;
+        when(medicineService.checkId(medId)).thenReturn(false);
+        when(medicineService.deleteMedicine(medId)).thenReturn(-1);
+        mockMvc.perform(delete("/admin/structure/medicine").param("medId", "101")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(404));
+    }
+    @Test
+    void happy_path_when_update_medicine() throws Exception {
+        int medId = 101;
+        Map param = new HashMap();
+        String medName = "med1";
+        String medDescrip = "des1";
+        param.put("medName", medName);
+        param.put("medDescrip", medDescrip);
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+        String json = objectWriter.writeValueAsString(param);
+
+        Medicine medicine = new Medicine(101, "vaccine1", "descriptiontest1");
+        when(medicineService.getMedicine(medId)).thenReturn(medicine);
+        mockMvc.perform(post("/admin/structure/medicine/101").content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void null_path_when_update_medicine() throws Exception {
+        int medId = 101;
+        Map param = new HashMap();
+        String medName = "med1";
+        String medDescrip = "des1";
+        param.put("medName", medName);
+        param.put("medDescrip", medDescrip);
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+        String json = objectWriter.writeValueAsString(param);
+
+        Medicine medicine = new Medicine(101, "vaccine1", "descriptiontest1");
+        when(medicineService.getMedicine(medId)).thenReturn(null);
+        mockMvc.perform(post("/admin/structure/medicine/101").content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(404));
+
+    }
+
 }
